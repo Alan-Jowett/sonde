20689185af04a7f16945a4b92a8b8b563bd9ea47
Alan Jowett <alan.jowett@microsoft.com>
2026-04-08 11:01:50 -0700

    Remove old v1 hardware generation prompts
    Signed-off-by: Alan Jowett <alan.jowett@microsoft.com>

--- hw/sonde_hw/__init__.py
+++
@@ -1,6 +0,0 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2026 sonde contributors
-
-"""sonde-hw: KiCad schematic generator from YAML board configurations."""
-
-__version__ = "0.1.0"
--- hw/sonde_hw/spice/__init__.py
+++
@@ -1,4 +0,0 @@
-# SPDX-License-Identifier: MIT
-# Copyright (c) 2026 sonde contributors
-
-"""SPICE simulation pipeline for sonde-hw."""
